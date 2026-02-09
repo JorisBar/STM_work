35829baab24933553c42703e94c3114794ac42c8
JorisBar <jorisbarauskasjoris@gmail.com>
2026-02-09 21:06:14 +0200

    Added GPIO and EXTI to f030

--- STM32f030_test/Src/main.c
+++ STM32f030_test/Src/main.c
@@ -9,6 +9,10 @@
 
 void SystemClock_Config(void);
 void GPIO_Init();
+void EXTI_Init();
+void EXTI_4_15_CallBack();
+
+volatile uint16_t period;
 
 int main(void)
 {
@@ -16,19 +20,22 @@ int main(void)
 
   SystemClock_Config();
   GPIO_Init();
+  EXTI_Init();
   SysTick->CTRL |= (0x01);
   SysTick->LOAD |= (0x00FFFFFF);
 
   uint32_t lastTimerValue = SysTick->VAL;
   uint32_t currentTimerValue;
-
+  period = 1000;
   while (1)
   {
+
 	  currentTimerValue = SysTick->VAL;
-	  if(lastTimerValue - currentTimerValue >= 1000*1000){
+	  if(lastTimerValue - currentTimerValue >= 1000*period){
 		  lastTimerValue = currentTimerValue;
 		  GPIOC->ODR ^= (1 << 14);
 	   	  }
+
 
   }
 
@@ -38,14 +45,41 @@ int main(void)
 void SystemClock_Config(void)
 {
 	RCC->CR |= (1);
-	RCC->AHBENR |= (1<<19);
+	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
 
 }
 
 void GPIO_Init(){
 
-	GPIOC->MODER |= (1<<28);
+	GPIOC->MODER |= (1 << 28);
+	GPIOC->PUPDR |= (1 << 30);
 
 
 }
+void EXTI_Init(){
+
+	EXTI->IMR |= (1 << 15);
+	EXTI->FTSR |= (1 << 15);
+
+	SYSCFG->EXTICR[3] |= (1 << 13);
+	EXTI->PR = (1<<15);
+
+	NVIC_EnableIRQ(EXTI4_15_IRQn);
+
+}
+
+void EXTI4_15_IRQHandler(){
+
+	EXTI->PR = (1<<15);
+	EXTI_4_15_CallBack();
+
+}
+
+void EXTI_4_15_CallBack(){
+	period /= 2;
+
+}
+
+
 
