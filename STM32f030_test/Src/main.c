12ba70e19d6853f53ee13b5a77ce42fea93c79a3
JorisBar <jorisbarauskasjoris@gmail.com>
2026-02-12 21:00:50 +0200

    started RTC

--- STM32f030_test/Src/main.c
+++ STM32f030_test/Src/main.c
@@ -10,6 +10,7 @@
 void SystemClock_Config(void);
 void GPIO_Init();
 void EXTI_Init();
+void RTC_Init();
 void EXTI_4_15_CallBack();
 
 volatile uint16_t period;
@@ -21,6 +22,7 @@ int main(void)
   SystemClock_Config();
   GPIO_Init();
   EXTI_Init();
+  RTC_Init();
   SysTick->CTRL |= (0x01);
   SysTick->LOAD |= (0x00FFFFFF);
 
@@ -77,7 +79,20 @@ void EXTI4_15_IRQHandler(){
 }
 
 void EXTI_4_15_CallBack(){
-	period /= 2;
+	GPIOC->ODR |= (1 << 14);
+
+}
+
+void RTC_Init(){
+	RTC->WPR = 0xCA;
+	RTC->WPR = 0x53;
+
+	RTC->ALRMASSR |= (8 << RTC_ALRMASSR_MASKSS_Pos);
+
+	RTC->PRER |= (0x7F << RTC_PRER_PREDIV_A_Pos);
+	RTC->PRER |= (0xFF << RTC_PRER_PREDIV_S_Pos);
+	RTC->CR |= RTC_CR_ALRAE;
+
 
 }
 
