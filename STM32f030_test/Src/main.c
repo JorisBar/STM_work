460ccab2f8570fe50e0c0f3a3c1cceb0ba7f4bfc
JorisBarLapTop <jorisbarauskasjoris@gmail.com>
2026-02-15 13:09:55 +0200

    working on RTC

--- STM32f030_test/Src/main.c
+++ STM32f030_test/Src/main.c
@@ -6,14 +6,21 @@
 
 
 
-
+uint8_t new_bcd_time(uint8_t delay);
 void SystemClock_Config(void);
 void GPIO_Init();
 void EXTI_Init();
 void RTC_Init();
 void EXTI_4_15_CallBack();
+void RTC_CallBack();
 
 volatile uint16_t period;
+volatile uint8_t bcd;
+volatile uint8_t seconds;
+
+
+volatile uint32_t debug_ALRMAR;
+volatile uint32_t debug_TR;
 
 int main(void)
 {
@@ -29,15 +36,19 @@ int main(void)
   uint32_t lastTimerValue = SysTick->VAL;
   uint32_t currentTimerValue;
   period = 1000;
+
   while (1)
   {
-
+	  /*
 	  currentTimerValue = SysTick->VAL;
 	  if(lastTimerValue - currentTimerValue >= 1000*period){
 		  lastTimerValue = currentTimerValue;
 		  GPIOC->ODR ^= (1 << 14);
 	   	  }
+	*/
 
+	  debug_ALRMAR = RTC->ALRMAR;
+	  debug_TR = RTC->TR;
 
   }
 
@@ -49,6 +60,7 @@ void SystemClock_Config(void)
 	RCC->CR |= (1);
 	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+	RCC->BDCR |= RCC_BDCR_RTCEN|RCC_BDCR_RTCSEL_LSI;
 
 }
 
@@ -84,17 +96,71 @@ void EXTI_4_15_CallBack(){
 }
 
 void RTC_Init(){
+	debug_ALRMAR = RTC->ALRMAR;
+	debug_TR = RTC->TR;
+
 	RTC->WPR = 0xCA;
 	RTC->WPR = 0x53;
 
+	RTC->CR &= ~RTC_CR_ALRAE;
+	while(!(RTC->ISR & RTC_ISR_ALRAWF));
+
+	debug_ALRMAR = RTC->ALRMAR;
+	debug_TR = RTC->TR;
+
+	RTC->PRER = (0x7F << RTC_PRER_PREDIV_A_Pos);
+	RTC->PRER |= (0xFF << RTC_PRER_PREDIV_S_Pos);
+
+
+	RTC->ALRMAR |= RTC_ALRMAR_MSK4_Msk|RTC_ALRMAR_MSK3_Msk|RTC_ALRMAR_MSK2_Msk;
+
+	RTC->ALRMAR &= ~(RTC_ALRMAR_ST_Msk|RTC_ALRMAR_SU_Msk);
+	RTC->ALRMAR |= new_bcd_time(1);
+
+
+
+
 	RTC->ALRMASSR |= (8 << RTC_ALRMASSR_MASKSS_Pos);
 
-	RTC->PRER |= (0x7F << RTC_PRER_PREDIV_A_Pos);
-	RTC->PRER |= (0xFF << RTC_PRER_PREDIV_S_Pos);
+	RTC->ALRMASSR &= ~RTC_ALRMASSR_SS_Msk;
+	RTC->ALRMASSR |= RTC->SSR;
+
+
+
+
 	RTC->CR |= RTC_CR_ALRAE;
+
+	NVIC_EnableIRQ(RTC_IRQn);
+}
+
+void RTC_IRQHandler(){
+	RTC->ISR |= RTC_ISR_ALRAF;
+
+	RTC_CallBack();
 
 
 }
+void RTC_CallBack(){
 
+	RTC->CR &= ~RTC_CR_ALRAE;
+	while(!(RTC->ISR & RTC_ISR_ALRAWF));
+
+	RTC->ALRMAR &= ~(RTC_ALRMAR_ST_Msk|RTC_ALRMAR_SU_Msk);
+	RTC->ALRMAR |= new_bcd_time(1);
+	GPIOC->ODR ^= GPIO_ODR_14;
+	RTC->CR |= RTC_CR_ALRAE;
+
+}
+
+
+uint8_t new_bcd_time(uint8_t delay){
+	bcd = RTC->TR&(RTC_TR_ST_Msk|RTC_TR_SU_Msk);
+	seconds = (bcd >> 4)*10 + (bcd & 0xF);
+	seconds += delay;
+	if(seconds==60){
+		seconds=0;
+	}
+	return ((seconds/10) << 4)|(seconds % 10);
+}
 
 
